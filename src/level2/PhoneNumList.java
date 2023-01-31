58cebfa3c5bd942913f726329c27dfd912a86f85
nh9600 <nh9600@daum.net>
2023-01-31 20:47:24 +0900

    레벨2_전화번호 목록

--- src/level2/PhoneNumList.java
+++ src/level2/PhoneNumList.java
@@ -1,23 +1,20 @@
 package level2;
 
+import java.util.Arrays;
+
 public class PhoneNumList {
 	public boolean solution(String[] phoneBook) {
+		Arrays.sort(phoneBook);
 		for (int i = 0; i < phoneBook.length - 1; i++) {
-			for (int j = i + 1; j < phoneBook.length; j++) {
-				if (phoneBook[i].startsWith(phoneBook[j])) {
-					return false;
-				}
-				if (phoneBook[j].startsWith(phoneBook[i])) {
-					return false;
-				}
+			if (phoneBook[i + 1].startsWith(phoneBook[i])) {
+				return false;
 			}
 		}
 		return true;
-
 	}
 
 	public static void main(String[] args) {
-		String[] phone_book = { "119", "97674223", "1195524421" };
+		String[] phone_book = {"119", "97674223", "1195524421" };
 		PhoneNumList pn = new PhoneNumList();
 		System.out.println(pn.solution(phone_book));
 	}
